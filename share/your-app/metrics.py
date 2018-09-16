162637190f7a5328e8557072504cbde817cadd52
itwye <itwye@qq.com>
2018-09-16 09:10:56 +0800

    v1.0 version

--- share/your-app/metrics.py
+++ share/your-app/metrics.py
@@ -7,30 +7,30 @@ import socket
 import random
 from prometheus_client import Gauge, Counter, start_http_server
 
-appname = "order"
+appname = "your-app-name"
 hostname = socket.gethostname()
 
 # -------------------------
 # http_requests_total
 # -------------------------
 
-c = Counter('http_requests_total', 'total number of http requests for application by response status',["app","host","status"])
+c = Counter('http_requests_total', 'total number of http requests for application by response status',["app","status"])
 
 def httpRequestTotal():
-    c.labels(appname,hostname,"200").inc(random.randint(5,10))
-    c.labels(appname,hostname,"502").inc(random.randint(0,3))
-    c.labels(appname,hostname,"404").inc(random.randint(0,3))
+    c.labels(appname,"200").inc(random.randint(5,10))
+    c.labels(appname,"502").inc(random.randint(0,3))
+    c.labels(appname,"404").inc(random.randint(0,3))
 
 # ------------------------
 # app_used_memory
 # ------------------------
 
-g = Gauge('app_used_memory', 'used memory for applications',["app","host"])
+g = Gauge('app_used_memory', 'used memory for applications',["app"])
 
 def appUsedMemory():
-    g.labels(appname,hostname).set(1024*1024*100)   
-    g.labels(appname,hostname).dec(1024*1024*random.randint(0,100))   
-    g.labels(appname,hostname).inc(1024*1024*random.randint(0,100))  
+    g.labels(appname).set(1024*1024*100)   
+    g.labels(appname).dec(1024*1024*random.randint(0,100))   
+    g.labels(appname).inc(1024*1024*random.randint(0,100))  
 
 # -----------------------
 # main
@@ -38,7 +38,7 @@ def appUsedMemory():
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8001)
+    start_http_server(8000)
     # Generate some requests
     while True:
         httpRequestTotal()
